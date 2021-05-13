164b3934eafb935e56964ac4195eff738e755a64
Cauany Rodrigues <cauanyrodrigues01@gmail.com>
2021-05-13 10:53:26 -0300

    Update LocalVacinacao.java

--- Sistema De Vacinacao COVID19/LocalVacinacao.java
+++ Sistema De Vacinacao COVID19/LocalVacinacao.java
@@ -11,6 +11,7 @@ public class LocalVacinacao {
 	 * <b>nome</b> é o atributo que armazena o nome do Local de Vacinação
 	 */
 	private String nome;
+	
 	/**
 	 * <b>horarioAtendimento</b> é o atributo que armazena o horario de atendimento do Local de Vacinação.
 	 */
@@ -40,7 +41,17 @@ public class LocalVacinacao {
 	 * <b>Enfermeiro<b> é o atributo que armazena a lista de enfermeiros do Local de Vacinação.
 	 */
 	private ArrayList<Enfermeiro> enfermeiros;
-
+	
+	/**
+	 * <b>pessoas</b> é o atributo que armazena a lista de pessoas do Local de Vacinação.
+	 */
+	private ArrayList<Pessoa> pessoas;
+	
+	/**
+	 * <b>ampolas</b> é o atributo que armazena a lista de ampolas do Local de Vacinação.
+	 */
+	private ArrayList<Ampola> ampolas;
+	
 	/**
 	 * Métodos Construtores.
 	 * @param nome O valor do nome do Local de Vacinação.
@@ -55,11 +66,98 @@ public class LocalVacinacao {
 		this.horarioAtendimento = horarioAtendimento;
 		this.estoqueVacina = estoqueVacina;
 		this.cidade = cidade;
-		this.medicos = new ArrayList();
-		this.enfermeiros = new ArrayList();
-    //adicionar lista pessoas 
+		this.medicos = new ArrayList<Medico>();
+		this.enfermeiros = new ArrayList<Enfermeiro>();
+		this.pessoas = new ArrayList<Pessoa>();
+		this.ampolas = new ArrayList<Ampola>();
+	}
+	
+	/**
+	 * Método que repõe o estoque e preenche a lista de ampolas com a quantidade correspondente ao número do estoque.
+	 * @param numNovasAmpola - número que será somado com o número de ampolas já existentes. 
+	 * @param ampola - a lista de ampolas vai ter N ampolas do mesmo tipo da ampola passada como parêmetro.
+	 * @author Cauany Nunes Rodrigues
+	 */
+	public void calculaEstoque(int numNovasAmpola, Ampola ampola) {
+		
+		// novoEstoque será o resultado do número de estoque já existente mais o número de ampolas adicionais.
+		int novoEstoque = getEstoqueVacina() + numNovasAmpola;
+		// estoqueVacina recebe o novo número de estoque.
+		setEstoqueVacina(novoEstoque);
+		
+		// A lista de ampolas vai receber novas instâncias de Ampola.
+		// O número de loops vai corresponder com o parâmetro numNovasAmpola.
+		// A lista vai receber ampolas iguas à aquela passada como parâmetro.
+		for (int n = 0; n <= numNovasAmpola; n++) {
+			adicionaAmpola(new Ampola(ampola.getLote(), ampola.getValidade(), ampola.getPrazo(), ampola.getFabricante()));
+		}
 	}
 
+	/**
+	 * Método que retira uma ampola do LocalVacinacao caso a ampola tenha acabado.
+	 * @param ampola
+	 * @author Cauany Nunes Rodrigues
+	 */
+	public void analisaAmpola() {
+		
+		// A lista ampolas possue várias ampolas que quando recém criadas tem 10 doses.
+		// Quando o número de doses é igual a 0 implica que essa ampola acabou, então deve-se removê-la.
+		if(getAmpolas().get(0).getDoses() == 0) {
+			// Diminui o primeiro valor da lista ampolas de LocalVacinacao.
+			retiraAmpola();
+			// Diminui um valor também do estoque de vacina para manter atualizado.
+			setEstoqueVacina(getEstoqueVacina() - 1);
+		}
+	}
+	
+	/**
+	 * Método que retira a primeira Ampola da lista ampolas.
+	 * @author Cauany Nunes Rodrigues
+	 */
+	public void retiraAmpola() {
+		this.ampolas.remove(0);
+	}
+	
+	/**
+	 * Método que adiciona os elementos na lista de ampolas.
+	 * @author Cauany Nunes Rodrigues
+	 */
+	public void adicionaAmpola(Ampola ampola) {
+		this.ampolas.add(ampola);
+	}
+
+	/**
+	 * Método que soma os elementos na lista de Médicos.
+	 * @author Pedro Lobo Nascimento
+	 */
+	public void AdicionarMedico(Medico medico) {
+		this.medicos.add(medico);
+	}
+	  
+	/**
+	 * Método que soma os elementos na lista de Enfermeiro.
+	 * @author Pedro Lobo Nascimento
+	 */
+	public void AdicionarEnfermeiro(Enfermeiro enfermeiro) {
+		this.enfermeiros.add(enfermeiro);
+	}
+	
+	/**
+	 * Método que soma a lista de Médicos e Enfermeiros.
+	 * @author Pedro Lobo Nascimento
+	 */
+	public int CalcularProfissionaisSaude(){
+		return medicos.size()+enfermeiros.size();
+	}
+	  
+	/**
+	 * Método que soma os elementos na lista de Médicos.
+	 * @author Pedro Lobo Nascimento
+	 */
+	public void AdicionarPessoa(Pessoa pessoa) {
+		this.pessoas.add(pessoa);
+	}
+	
 	/**
 	 * Método que retorna o valor do nome do Local de Vacinação.
 	 * @return O nome do Local de Vacinação.
@@ -173,6 +271,38 @@ public class LocalVacinacao {
 	}
 
 	/**
+	 * Método que retorna as pessoas que fazem parte do Local de Vacinação.
+	 * @return A lista de pessoas do Local de Vacinação.
+	 */
+	public ArrayList<Ampola> getAmpolas() {
+		return ampolas;
+	}
+
+	/**
+	 * Método que altera o valor das ampolas do Local de Vacinação.
+	 * @param ampolas A nova lista de ampolas do Local de Vacinação.
+	 */
+	public void setAmpolas(ArrayList<Ampola> ampolas) {
+		this.ampolas = ampolas;
+	}
+	
+	/**
+	 * Método que retorna as pessoas que fazem parte do Local de Vacinação.
+	 * @return A lista de pessoas do Local de Vacinação.
+	 */
+	public ArrayList<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	/**
+	 * Método que altera o valor das pessoas do Local de Vacinação.
+	 * @param pessoas A nova lista de pessoas do Local de Vacinação.
+	 */
+	public void setPessoas(ArrayList<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+
+	/**
 	 * Método que compara se o objeto passado como parâmetro é do tipo LocalVacinacao.
 	 * Caso o seja, ele compara o nome duas classes LocalVacinacao.
 	 */
@@ -189,27 +319,7 @@ public class LocalVacinacao {
 	 * Método que retorna o local de vacinação e o horário de atendimento em String.
 	 */
 	public String toString() {
-		return "Local de vacinação:\nNome: "+getNome()+"\nHorario de atendimento: "+getHorarioAtendimento();
-	}
-
-  /**
-   * Método que soma os elementos na lista de Médicos.
-   */
-  public void AdicionarMedico(Medico medico) {
-		this.medicos.add(medico);
-	}
+		return "localizado em "+ getCidade() +"\nHorario de atendimento: "+getHorarioAtendimento();
+	} 
   
-  /**
-   * Método que soma os elementos na lista de Enfermeiro.
-   */
-  public void AdicionarEnfermeiro(Enfermeiro enfermeiro) {
-    this.enfermeiros.add(enfermeiro);
-  }
-  
-  /**
-   * Método que soma a lista de Médicos e Enfermeiros.
-   */
-  public int CalcularProfissionaisSaude(){
-    return medicos.size()+enfermeiros.size();
-  }
 }
