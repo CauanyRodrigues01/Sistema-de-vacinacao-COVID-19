5bd03865dbf52d0587eb26fc38508322d27162a9
Cauany Rodrigues <cauanyrodrigues01@gmail.com>
2021-05-13 10:53:10 -0300

    Update Cidade.java

--- Sistema De Vacinacao COVID19/Cidade.java
+++ Sistema De Vacinacao COVID19/Cidade.java
@@ -33,11 +33,6 @@ public class Cidade {
 	private int numPessoasImunizada;
 
 	/**
-	 * <b>pessoas</b> é o atributo que armazena a lista de pessoas na Cidade.
-	 */
-	private ArrayList<Pessoa>pessoas;
-
-	/**
 	 * <b>estoque</b> é o atributo que armazena o estoque de vacinas na Cidade.
 	 */
 	private int estoque;
@@ -51,13 +46,38 @@ public class Cidade {
 	 * @param estoque O valor do estoque na Cidade.
 	 * @param locaisVacinacao O valor dos locais de vacinação da Cidade.
 	 */
-	public Cidade(String nome, String UF, int numTotalPessoas, int numPessoasImunizada, int estoque) {
+	public Cidade(String nome, String UF, int numPessoasImunizada, int estoque) {
 		this.nome = nome;
 		this.UF = UF;
-		this.numTotalPessoas = numTotalPessoas;
 		this.numPessoasImunizada = numPessoasImunizada;
 		this.estoque = estoque;
 		this.locaisVacinacao =  new ArrayList<LocalVacinacao>();
+	}
+	
+	/**
+	 * Metodo que adiciona um local de vacinação dentro da lista de locais de vacinação da cidade.
+	 * @author Maria Eduarda Viana Cordeiro dos Santos.
+	 */
+	public void addLocalVacinacao(LocalVacinacao local){
+		this.locaisVacinacao.add(local);
+	}
+	
+	/**
+	 * Metodo que retorna quantas pessoas não estão imunizadas na cidade.
+	 * @author Maria Eduarda Viana Cordeiro dos Santos. 
+	 */
+	/*public int CalcularPessoaNaoImunizada() {
+		int pessoaNaoImunizadas = getNumTotalPessoas() - getNumPessoasImunizada();
+		return pessoaNaoImunizadas; 
+	}*/
+	
+	/**
+	 * Metodo que retorna quantos locais de vacinação existe na cidade.
+	 * @author Maria Eduarda Viana Cordeiro dos Santos.
+	 */
+	public int CalcularLocaisVacinacao() {
+		int NumLocaisVacinacao = locaisVacinacao.size();
+		return NumLocaisVacinacao;
 	}
 	
 	/**
@@ -90,22 +110,6 @@ public class Cidade {
 	 */
 	public void setUF(String uf) {
 		UF = uf;
-	}
-
-	/**
-	 * Método que retorna o número total de pessoas na Cidade.
-	 * @return O número total de pessoas na Cidade.
-	 */
-	public int getNumTotalPessoas() {
-		return numTotalPessoas;
-	}
-
-	/**
-	 * Método que altera o valor do atributo numTotalPessoas.
-	 * @param numTotalPessoas O novo número total de pessoas na Cidade.
-	 */
-	public void setNumTotalPessoas(int numTotalPessoas) {
-		this.numTotalPessoas = numTotalPessoas;
 	}
 
 	/**
@@ -170,36 +174,11 @@ public class Cidade {
 			return true;
 		return false;
 	}
-
-  /**
+	/**
 	 * Metodo que retorna o nome e UF da Cidade em String.
 	 */
 	public String toString() {
-		return "Cidade: "+getNome()+"-"+getUF();
-  }
-  /**
-  * Metodo que adiciona um local de vacinação dentro da lista de locais de vacinação da cidade
-  */
-    public void addLocalVacinacao(LocalVacinacao local){
-		  this.locaisVacinacao.add(local);
+		return getNome()+"-"+getUF();
 	}
-
-  /**
-  * Metodo que retorna quantas pessoas não estão imunizadas na cidade 
-  */
-  public int CalcularPessoaNaoImunizada(){
-		int pessoaNaoImunizadas = getNumTotalPessoas() - getNumPessoasImunizada();
-		return pessoaNaoImunizadas; 
-	}
-
-   /**
-  * Metodo que retorna quantos locais de vacinação existe na cidade
-  */
-  public int CalcularLocaisVacinacao() {
-		int NumLocaisVacinacao = locaisVacinacao.size();
-		return NumLocaisVacinacao;
-	}
-
-
-
+	
 }
